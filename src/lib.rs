f0e40c6b75ed5e1760f8f7692cb2bebb02a44421
Gustav Hagland <gustav.hagland@gmail.com>
2023-09-21 20:03:44 +0200

    Started even more!

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,52 @@
 use std::io::StdinLock;
 
 use serde::{Deserialize, Serialize};
-use serde_json::from_reader;
+use serde_json::{from_reader, to_string};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Message<Body> {
+struct Message {
     src: String,
     dest: String,
-    body: Body,
+    body: MessageBody,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MessageBody {
+    msg_id: i32,
+    in_reply_to: i32,
+    #[serde(flatten)]
+    body: MessageContent,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
-enum Messages {
-    #[serde(rename = "init")]
-    Init {},
+#[serde(rename_all = "snake_case")]
+enum MessageContent {
+    Init,
+    InitOk,
 }
 
 pub fn run() {
-    while let Some(message) =
-        from_reader::<StdinLock, Message<Messages>>(std::io::stdin().lock()).ok()
-    {}
+    while let Some(message) = from_reader::<StdinLock, Message>(std::io::stdin().lock()).ok() {
+        let result = handle_message(&message.body.body);
+        for message_content in result.output {
+            let return_message = Message {
+                src: 
+            }
+            println!("{}", to_string(&message).unwrap());
+        }
+    }
+}
+
+struct HandleResult {
+    output: Vec<MessageContent>,
+}
+
+fn handle_message(message: &MessageContent) -> HandleResult {
+    match message {
+        MessageContent::Init => HandleResult {
+            output: vec![MessageContent::InitOk],
+        },
+        MessageContent::InitOk => todo!(),
+    }
 }
