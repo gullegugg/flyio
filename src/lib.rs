486897208ffcee8a99b78ee6f94a53a3aa5e9dcc
Gustav Hagland <gustav.hagland@vermiculus.se>
2023-09-25 21:22:00 +0200

    To tired for lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,115 @@
-use std::{fmt::Error, io::StdinLock};
+use std::io::StdinLock;
 
 use serde::{Deserialize, Serialize};
-use serde_json::{from_reader, to_string};
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Message {
-    src: String,
-    dest: String,
-    body: MessageBody,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct MessageBody {
-    msg_id: Option<i32>,
-    in_reply_to: Option<i32>,
-    #[serde(flatten)]
-    body: MessageContent,
-}
+use serde_json::from_reader;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-enum MessageContent {
+enum MessageContent<'a> {
     Init {
-        node_id: String,
-        node_ids: Vec<String>,
+        node_id: &'a str,
+        node_ids: Vec<&'a str>,
     },
     InitOk,
 }
 
-pub fn run() {
-    let mut node_id = "".to_string();
-    let mut all_nodes: Vec<String> = vec![];
+#[derive(Debug, Serialize, Deserialize)]
+struct MessageBody<'a> {
+    msg_id: Option<i32>,
+    in_reply_to: Option<i32>,
+    #[serde(flatten)]
+    body: MessageContent<'a>,
+}
 
-    loop {
-        let message = from_reader::<StdinLock, Message>(std::io::stdin().lock()).unwrap();
-        match message.body.body {
-            MessageContent::Init {
-                node_id: new_node_id,
-                node_ids: new_node_ids,
-            } => {
-                node_id = new_node_id;
-                all_nodes = new_node_ids;
-                let return_message = Message {
-                    src: node_id.clone(),
-                    dest: message.src.clone(),
-                    body: MessageBody {
-                        msg_id: None,
-                        in_reply_to: None,
-                        body: MessageContent::InitOk,
-                    },
-                };
-                match serde_json::to_string(&return_message) {
-                    Ok(string) => println!("{}", string),
-                    Err(err) => eprintln!("{}", err),
-                }
-            }
-            MessageContent::InitOk => eprintln!("Init failed"),
-        }
+#[derive(Debug, Serialize, Deserialize)]
+struct Message<'a> {
+    src: &'a str,
+    dest: &'a str,
+    body: MessageBody<'a>,
+}
+
+trait Publisher<'a> {
+    fn publish(&self, message: &Message<'a>);
+}
+
+struct JsonSender;
+
+impl Publisher<'_> for JsonSender {
+    fn publish(&self, message: &Message) {
+        let str_val = serde_json::to_string(message).unwrap();
+        println!("{}", str_val)
     }
 }
 
-// Börjar helt stateless
-// Init ger state: nodes och node id
-// Kan ha som map? Eller lättast med bara variabler i struct.
-// Stöd för transactionellt? Eller lättast att bara gör allt direkt.
-// Vore bra med abstraction över skicka meddelande och loggning.
-
-// Kan också se init som en helt annan grej. Då vet vi att det alltid finns ett node id.
-
-struct Node<MessageSender: Fn(Message)> {
+struct Node<P: Publisher> {
     node_id: Option<String>,
-    send_message: MessageSender,
+    send_message: P,
 }
 
-impl<MessageSender: Fn(Message)> Node<MessageSender> {
-    fn new(sender: MessageSender) -> Self {
+impl<P: Publisher> Node<P> {
+    fn new(sender: P) -> Self {
         Node {
             node_id: None,
             send_message: sender,
         }
+    }
+
+    fn handle_message(&mut self, message: Message) {
+        let content: Option<MessageContent> = match message.body.body {
+            MessageContent::Init {
+                node_id,
+                node_ids: _,
+            } => {
+                self.node_id = Some(node_id.to_string());
+                Some(MessageContent::InitOk)
+            }
+            MessageContent::InitOk => {
+                eprintln!("Why did i receive init ok?");
+                None
+            }
+        };
+        if let Some(response) = content {
+            self.respond_message(&message, response);
+        }
+    }
+
+    fn respond_message(&self, origin: &Message, message_content: MessageContent) {
+        self.send_message.publish(&Message {
+            src: &self.node_id.unwrap(),
+            dest: origin.src.clone(),
+            body: MessageBody {
+                msg_id: None,
+                in_reply_to: origin.body.msg_id,
+                body: message_content,
+            },
+        })
+    }
+}
+
+struct CollectPublisher<'a> {
+    published_messages: Vec<&'a Message>,
+}
+
+impl CollectPublisher<'_> {
+    fn new() -> Self {
+        CollectPublisher {
+            published_messages: Vec::new(),
+        }
+    }
+}
+
+impl<'a> Publisher<'a> for &mut CollectPublisher<'a> {
+    fn publish(&self, message: &Message<'a>) {
+        self.published_messages.push(message);
+    }
+}
+
+pub fn run() {
+    let mut node = Node::new(JsonSender);
+    loop {
+        let message = from_reader::<StdinLock, Message>(std::io::stdin().lock()).unwrap();
+        node.handle_message(message);
     }
 }
 
@@ -90,7 +120,28 @@ mod test {
     #[test]
     fn verify_init() {
         // Given
-        let mut messages: Vec<Message> = vec![];
-        let mut node = Node::new(|msg| messages.push(msg));
+        let mut publisher = CollectPublisher::new();
+        let mut node = Node::new(&mut publisher);
+
+        let node_id = "n2".to_string();
+
+        let message = Message {
+            src: "n1".to_string(),
+            dest: "n2".to_string(),
+            body: MessageBody {
+                msg_id: Some(1),
+                in_reply_to: None,
+                body: MessageContent::Init {
+                    node_id: node_id.clone(),
+                    node_ids: vec!["n1".to_string(), "n2".to_string()],
+                },
+            },
+        };
+        // When
+        node.handle_message(message);
+
+        // Then
+        assert_eq!(node.node_id, Some(node_id));
+        assert_eq!(publisher.published_messages.len(), 1);
     }
 }
